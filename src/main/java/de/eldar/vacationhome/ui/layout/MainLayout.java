08c0c8af2adb10cda6ad1c369b86d2a83ac913df
Torsten Kehrer <kehrer.torsten@gmail.com>
2018-08-05 18:56:11 +0200

    Cleanup

--- src/main/java/de/eldar/vacationhome/ui/layout/MainLayout.java
+++ src/main/java/de/eldar/vacationhome/ui/layout/MainLayout.java
@@ -22,31 +22,9 @@ import de.eldar.vacationhome.ui.route.WelcomeRoute;
 @Tag("main-layout")
 public class MainLayout extends PolymerTemplate<MainLayoutModel>  implements RouterLayout{
 
-	private static final String HERO_TEXT_CSS_CLASS = "hero-text";
-
-	private static final String HERO_IMAGE_CSS_CLASS = "hero-image";
-
-	private static final String HERO_CSS_CLASS = "hero";
-
-	private static final String FOOTER_CSS_CLASS = "footer";
-
-	private static final String FOOTER_NAME_CSS_CLASS = "footer-name";
-
-	private static final String CONTENT_CSS_CLASS = "content";
-
-	private static final String NAVIGATION_CSS_CLASS = "header_navigation";
-
-	private static final String HEADER_CSS_CLASS = "header";
-
 	private static final String NAV_ITEM_CSS_CLASS = "header_nav-item";
 
-	private static final String MAIN_CSS_CLASS = "main-layout";
-
 	private static final long serialVersionUID = 661089314620602587L;
-
-//	private Div headerContainer;
-//
-//	private Div heroContainer;
 	
 	@Id("navigationContainer")
 	private Div navigationContainer;
@@ -62,52 +40,11 @@ public class MainLayout extends PolymerTemplate<MainLayoutModel>  implements Rou
 	}
 	
 	public MainLayout() {
-//		addClassName(MAIN_CSS_CLASS);
-//		createHeaderContainer();
-//		createHeroContainer();
-//		createContentContainerWithFooter();		
-//		add(new CookieConsent(),headerContainer,heroContainer,contentContainer);
-		createNavigationContainer();
+		initNavigationContainer();
 		getModel().setHeroText("Vaadin vacation home hero text.");
 	}
-	
-//	private void createContentContainerWithFooter() {
-//		contentContainer = new Div();
-//		contentContainer.setClassName(CONTENT_CSS_CLASS);
-//		
-//		Paragraph footerName = new Paragraph("Vaadin vacation home \u00A9 2018");
-//		footerName.setClassName(FOOTER_NAME_CSS_CLASS);
-//
-//		Anchor scrollUP = new Anchor();
-//		scrollUP.setTarget("_top");
-//		scrollUP.setText("Back to the top!");
-//		scrollUP.setHref("#");
-//		
-//		footerContainer = new Div(scrollUP,footerName);
-//		footerContainer.setClassName(FOOTER_CSS_CLASS);
-//		contentContainer.add(footerContainer);
-//	}
-//
-//	private void createHeroContainer() {
-//		heroContainer = new Div();
-//		heroContainer.setClassName(HERO_CSS_CLASS);
-//		
-//		Div heroImageContainer = new Div();
-//		heroImageContainer.setClassName(HERO_IMAGE_CSS_CLASS);
-//		Div heroTextContainer = new Div();
-//		heroTextContainer.setClassName(HERO_TEXT_CSS_CLASS);		
-//		heroTextContainer.add(new H1("Vaadin vacation home overlay text"));
-//		heroContainer.add(heroImageContainer,heroTextContainer);
-//	}
-//
-//	private void createHeaderContainer() {
-//		Div navigationContainer = createNavigationContainer();
-//		headerContainer = new Div();
-//		headerContainer.setClassName(HEADER_CSS_CLASS);	
-//		headerContainer.add(navigationContainer);
-//	}
 
-	private Div createNavigationContainer() {
+	private Div initNavigationContainer() {
 		RouterLink guestBookLink = new RouterLink(null, GuestbookRoute.class);
 		guestBookLink.add(new Icon(VaadinIcon.LIST), new Text("Guestbook"));
 		guestBookLink.addClassName(NAV_ITEM_CSS_CLASS);
